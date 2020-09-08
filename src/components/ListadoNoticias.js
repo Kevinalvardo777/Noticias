f25e9a1db7627720c8719c4b18d1f60b2afd29a9
kevin <kevinalvapearson777@gmail.com>
2020-09-08 00:41:26 -0500

    Mostrando las noticias

--- src/components/ListadoNoticias.js
+++ src/components/ListadoNoticias.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Noticia from './Noticia';
+
 const ListadoNoticias = ({noticias}) => {
     return ( 
         <div className="row">
             {noticias.map(noticia => (
-                <Noticia />
+                <Noticia 
+                    key= {noticia.url}
+                    noticia = {noticia}
+                />
             ))}
         </div>
      );
